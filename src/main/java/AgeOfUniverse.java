efe80aa11d9dff5ac6047d978f7daf234ffdf2fa
Homework1 <kalinina_en@hotmail.com>
2023-10-30 22:43:49 +0300

    Initial commit

---
+++ src/main/java/AgeOfUniverse.java
@@ -0,0 +1,14 @@
+public class AgeOfUniverse {
+
+    public static void main(String[] args) {
+
+        final long ageOfUniverse = 13000000000L;
+        System.out.println("ageOfUniverse = " + ageOfUniverse);
+
+        final int ageOfHumanity = 100000;
+        System.out.println("ageOfHumanity = " + ageOfHumanity);
+
+        final long ageDifference = ageOfUniverse / ageOfHumanity;
+        System.out.println("ageDifference = " + ageDifference);
+    }
+}
